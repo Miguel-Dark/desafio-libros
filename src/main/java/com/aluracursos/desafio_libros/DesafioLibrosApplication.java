9cd663b8cdb6b97827dc175d92962785a598342e
Miguel Ángel de la Cruz Lázaro <256243310+Miguel-Dark@users.noreply.github.com>
2026-02-03 05:06:43 -0600

    first commit: Estructura inicial y ConsumoAPI

---
+++ src/main/java/com/aluracursos/desafio_libros/DesafioLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.desafio_libros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioLibrosApplication.class, args);
+	}
+
+}
