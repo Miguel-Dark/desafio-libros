6ff07a6c8cbf981e7bef94d6aa7b5ee86257c20e
Miguel Ángel de la Cruz Lázaro <256243310+Miguel-Dark@users.noreply.github.com>
2026-02-04 06:03:48 -0600

    feat: implementar filtrado de Top 10 libros más descargados
    Se integra el uso de Java Streams y Comparator para ordenar los resultados de la API por conteo de descargas en orden descendente, limitando la salida a los 10 mejores títulos en mayúsculas.

--- src/main/java/com/aluracursos/desafio_libros/principal/Principal.java
+++ src/main/java/com/aluracursos/desafio_libros/principal/Principal.java
@@ -1,8 +1,12 @@
 package com.aluracursos.desafio_libros.principal;
 
+import ch.qos.logback.core.encoder.JsonEscapeUtil;
 import com.aluracursos.desafio_libros.model.Datos;
+import com.aluracursos.desafio_libros.model.DatosLibros;
 import com.aluracursos.desafio_libros.service.ConsumoAPI;
 import com.aluracursos.desafio_libros.service.ConvierteDatos;
+
+import java.util.Comparator;
 
 public class Principal {
     private static final String URL_BASE = "https://gutendex.com/books/";
@@ -13,5 +17,13 @@ public class Principal {
         System.out.println(json);
         var datos = conversor.obtenerDatos(json,Datos.class);
         System.out.println(datos);
+
+        //Top 10 libros más descargados
+        System.out.println("Top 10 libros más descargados");
+        datos.resultados().stream()
+                .sorted(Comparator.comparing(DatosLibros::numeroDeDescargas).reversed())
+                .limit(10)
+                .map(l -> l.titulo().toUpperCase())
+                .forEach(System.out::println);
     }
 }
